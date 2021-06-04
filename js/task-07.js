cd60907f531e5abcd9bbc33a9ca14ceea02f7d5d
Olena Avdiunina <elena.avdiunina@gmail.com>
2021-06-04 17:36:14 +0300

    fix 7

--- js/task-07.js
+++ js/task-07.js
@@ -7,7 +7,7 @@
 
 const inputRangeRef = document.querySelector("#font-size-control");
 const inputTextRef = document.querySelector("#text");
+inputTextRef.style.fontSize = `${inputRangeRef.value}px`;
 const handleInput = () => {
-  inputTextRef.style.fontSize = `${inputRangeRef.value}px`;
+  inputRangeRef.addEventListener("input", handleInput);
 };
-inputRangeRef.addEventListener("input", handleInput);
