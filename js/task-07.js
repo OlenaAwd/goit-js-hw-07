815b991be9489bf4e7264f0ac02a713bc1dbcbe4
Igor Avdiunin <igor.avdiunin@gmail.com>
2021-06-06 09:21:48 +0300

    fix 7

--- js/task-07.js
+++ js/task-07.js
@@ -9,5 +9,6 @@ const inputRangeRef = document.querySelector("#font-size-control");
 const inputTextRef = document.querySelector("#text");
 inputTextRef.style.fontSize = `${inputRangeRef.value}px`;
 const handleInput = () => {
-  inputRangeRef.addEventListener("input", handleInput);
+  inputTextRef.style.fontSize = `${inputRangeRef.value}px`;
 };
+  inputRangeRef.addEventListener("input", handleInput);
